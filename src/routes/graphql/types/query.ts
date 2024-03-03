47af85c89a96421bfa57459332edc0b093fa36e6
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-03-03 16:59:20 +0500

    feat: profiles, memberTypes requests

--- src/routes/graphql/types/query.ts
+++ src/routes/graphql/types/query.ts
@@ -2,6 +2,8 @@ import { GraphQLList, GraphQLObjectType } from 'graphql';
 import { PrismaClient } from '@prisma/client';
 import { PostType } from './post.js';
 import { UserType } from './user.js';
+import { ProfileType } from './profile.js';
+import { MemberType } from './memberType.js';
 
 export const QueryType = new GraphQLObjectType({
   name: 'Query',
@@ -19,6 +21,21 @@ export const QueryType = new GraphQLObjectType({
       resolve: async (_, args, contex: PrismaClient) => {
         return await contex.user.findMany();
       },
-    }
+    },
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      description: 'Profiles query',
+      resolve: async (_, args, contex: PrismaClient) => {
+        console.log('here');
+        return await contex.profile.findMany();
+      },
+    },
+    memberTypes: {
+      type: new GraphQLList(MemberType),
+      description: 'MemberTypes query',
+      resolve: async (_, args, contex: PrismaClient) => {
+        return await contex.memberType.findMany();
+      },
+    },
   },
 });
